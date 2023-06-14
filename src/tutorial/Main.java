545ba1323d18040152b58c3eee867358fe29ee47
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-06-14 12:40:29 +0530

    practicing with Proxy class methods

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -4,17 +4,20 @@ import java.lang.reflect.Proxy;
 
 public class Main {
     public static void main(String[] args) {
-        // Create the original object
         Man ron = new Man("Ron", 15, "Devon", "England");
-//        Get the class loader from the original object
         ClassLoader ronClassLoader = ron.getClass().getClassLoader();
-//        Get all interfaces that the original object implements
         Class[] interfaces = ron.getClass().getInterfaces();
-//        Create a proxy for ron object
+
         Person proxyRon = (Person) Proxy.newProxyInstance(ronClassLoader, interfaces, new PersonInvocationHandler(ron));
-//        Call one of our original object's methods on the proxy object
         proxyRon.introduce(ron.getName());
         proxyRon.sayAge(ron.getAge());
         proxyRon.sayWhereFrom(ron.getCity(), ron.getCountry());
+        System.out.println();
+
+        Person proxyRon1 = (Person) Proxy.newProxyInstance(ronClassLoader, new Class[]{Person.class}, new PersonInvocationHandler(ron));
+        proxyRon1.introduce(ron.getName());
+
+        System.out.println(Proxy.isProxyClass(ron.getClass()));
+        System.out.println(Proxy.isProxyClass(proxyRon.getClass()));
     }
 }
